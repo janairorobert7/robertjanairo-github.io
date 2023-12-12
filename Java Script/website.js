55a52198f66c4b05f9a2b0c39531c4f153468d10
janairorobert7 <146044875+janairorobert7@users.noreply.github.com>
2023-12-11 18:30:43 -0800

    Add files via upload

---
+++ Java Script/website.js
@@ -0,0 +1,8 @@
+/*Typing animation*/
+
+var typed = new Typed(".typing", {
+    strings: ["Musican", "Artist", "Singer", "Baker", "Web Developer"],
+    typeSpeed: 100,
+    BackSpeed: 60,
+    loop:true
+})
